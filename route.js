19eaf9a7c628f3a9114c43e5433313708f2ae831
Blockchain | Game | Backend Engineer (DreamMicro) <backendeng0417@gmail.com>
2023-10-15 01:20:08 -0800

    updatedd

--- route.js
+++ route.js
@@ -9,7 +9,7 @@ userRouter.get("/test", (req, res) => {
 })
 userRouter.post("/nftsave", NFTSave);
 userRouter.post("/nftget", NFTSGet);
-userRouter.get("/getcids", GetCids);
+userRouter.post("/getcids", GetCids);
 userRouter.get("/getmetadatas", GetMetadatas);
 userRouter.post("/getinventory", GetInventory);
 
