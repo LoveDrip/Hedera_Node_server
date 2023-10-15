d8e0d87c4b7f2ea60b983d43fc766457d9b00902
Blockchain | Game | Backend Engineer (DreamMicro) <backendeng0417@gmail.com>
2023-10-14 17:51:53 -0800

    sdf

--- nft.js
+++ nft.js
@@ -46,37 +46,37 @@ const NFTSGet = async (req, res) => {
 };
 
 const GetCids = async (req, res) => {
-  console.log("CIDS");
-  const cids = [
-    "bafkreieoyfefyak7xzarvwojuljsigqkjja2clawycsb2y77smcwj6u25e",
-    "bafkreibywazlivhx3tvkpxllcrjxmlihqp4ch7yl34qh3i5vp53mzsxfmy",
-    "bafkreidad2mopo5nz2ziz44dmp4axxjscbr65rsbcjnnsw4tye4y53dnfm",
-    "bafkreifzs4sfs6s2wjeeaq7sxce4u5icyty47kgmrs4kazqycrz4nicnfi",
-    "bafkreign4fovo6zbtagj6zupiemetyxijkej3jxy5z2ysg4j377jphvu4u",
-    "bafkreigyueqf44odnkjhi7zl4up4s74ui6wi5amzlcv7bptznps74f67ei",
-    "bafkreieg33hdavuz63l6q3dvnjqh6myf4z2fskri46upj745xdax7i7s4u",
-  ];
+  // console.log("CIDS");
+  // const cids = [
+  //   "bafkreieoyfefyak7xzarvwojuljsigqkjja2clawycsb2y77smcwj6u25e",
+  //   "bafkreibywazlivhx3tvkpxllcrjxmlihqp4ch7yl34qh3i5vp53mzsxfmy",
+  //   "bafkreidad2mopo5nz2ziz44dmp4axxjscbr65rsbcjnnsw4tye4y53dnfm",
+  //   "bafkreifzs4sfs6s2wjeeaq7sxce4u5icyty47kgmrs4kazqycrz4nicnfi",
+  //   "bafkreign4fovo6zbtagj6zupiemetyxijkej3jxy5z2ysg4j377jphvu4u",
+  //   "bafkreigyueqf44odnkjhi7zl4up4s74ui6wi5amzlcv7bptznps74f67ei",
+  //   "bafkreieg33hdavuz63l6q3dvnjqh6myf4z2fskri46upj745xdax7i7s4u",
+  // ];
 
-  const CID = new CIDS({
-    Metadata: cids 
-  })
-
-  CID.save()
-  // console.log("sdsdsd")
-  // CIDS.find().then(async (cids) => {
-  //   const arr = cids[0].Metadata;
-  //   const metadata = [];
-  //   for (var i = 0; i < arr.length; i++) {
-  //     const newValue = Buffer.from(arr[i]);
-  //     let cid = arr[i]
-  //     const res = await fetch(`https://ipfs.io/ipfs/${newValue}`);
-  //     const meta = await res.json();
-  //     meta.cid = cid
-  //     metadata[i] = meta;
-  //   }
-  //   // console.log(metadata)
-  //   res.send(metadata)
+  // const CID = new CIDS({
+  //   Metadata: cids 
   // })
+
+  //  CID.save()
+  console.log("sdsdsd")
+  CIDS.find().then(async (cids) => {
+    const arr = cids[0].Metadata;
+    const metadata = [];
+    for (var i = 0; i < arr.length; i++) {
+      const newValue = Buffer.from(arr[i]);
+      let cid = arr[i]
+      const res = await fetch(`https://ipfs.io/ipfs/${newValue}`);
+      const meta = await res.json();
+      meta.cid = cid
+      metadata[i] = meta;
+    }
+    // console.log(metadata)
+    res.send(metadata)
+  })
 
 }
 
