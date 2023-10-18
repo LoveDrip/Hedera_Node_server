bf004eb12c39c31f1055e3ff3968b8bee0774bb2
Blockchain | Game | Backend Engineer (DreamMicro) <backendeng0417@gmail.com>
2023-10-18 11:33:11 +0000

    add frontend

--- Model/NFTs.js
+++ Model/NFTs.js
@@ -1,12 +1,6 @@
 const mongoose = require("mongoose");   
 
 const NFTs = new mongoose.Schema({
-    // AccountId: {
-    //     type: String
-    // },
-    // Price: {
-    //     type: String
-    // },
     Metadata: {
         type: Array
     }
