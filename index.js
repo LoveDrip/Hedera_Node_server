b21355ada8e1114853060e767230e90d4889095c
lovedrip <richbondjames@gmail.com>
2023-11-04 05:19:49 -0700

    cert

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/v3market.net/privkey.pem', 'utf8'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/v3market.net/fullchain.pem', 'utf8')
+  key: fs.readFileSync('selfsigned.key', 'utf8'),
+  cert: fs.readFileSync('selfsigned.crt', 'utf8')
 }
 
 app.use("/users", userRouter);
